6b7950b25134c3db6f533bc86f2f6d3a86ce673e
Ledger Heath <laored@outlook.com>
2024-06-05 00:09:05 +0800

    init

--- DataRetention/DataRetention/DataRetention/ViewController.m
+++ DataRetention/DataRetention/DataRetention/ViewController.m
@@ -16,6 +16,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    [defaults setObject:[NSDate date] forKey:@"LastLoginTime"];
+    [defaults setBool:NO forKey:@"IsFirstLogin"];
+    [defaults setValue:@"ledger" forKey:@"UserName"];
+    
+    [defaults synchronize];
+    
+    NSDate *lastLoginTime = [defaults objectForKey:@"LastLoginTime"];
+    BOOL isFirstLogin = [defaults boolForKey:@"IsFirstLogin"];
+    NSString *userName = [defaults valueForKey:@"UserName"];
+    
+    NSLog(@"%@--%d--%@", lastLoginTime, isFirstLogin, userName);
+
+    [defaults removeObjectForKey:@"LastLoginTime"];
+    
+    NSLog(@"%@--%d--%@", lastLoginTime, isFirstLogin, userName);
+
 }
 
 
---
+++ Data_Persistence/Data_Persistence/Data_Persistence/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Data_Persistence
+//
+//  Created by Ledger Heath on 2024/6/4.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
